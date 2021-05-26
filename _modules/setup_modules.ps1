dbc8cd9065ae48c0e2610521c0b313cbd751aa95
Andreas Felix Häberle <anfe@Hannas-MacBook.fritz.box>
2021-05-26 06:25:22 +0200

    added first two submodules mdpre and md2pptx from Martin P.

---
+++ _modules/setup_modules.ps1
@@ -0,0 +1,12 @@
+python3 -m pip install --upgrade pip 
+python3 -m pip install lxml
+python3 -m pip install pillow
+python3 -m pip install XlsxWriter
+python3 -m pip install python-pptx
+python3 -m pip install python-docx
+
+python3 -m pip install loguru
+
+
+
+
